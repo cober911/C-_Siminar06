cf2d3ee6728f16cb34127b1a12432423bb0000fd
VladimirE <wortex2014@bk.ru>
2022-10-09 11:38:49 +0400

    ex3

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу которая будет преобразовывать десятичное число в двоичное */
+
+int GetNumber()
+{
+    
+    return Number;
+}
